67f6276d6a43637f9a707696c22d3d9b4ff09c66
Vladimir Panteleev <git@thecybershadow.net>
2019-03-11 23:45:47 +0000

    filter-*: Refactor graph definition to its own file

--- filter-with-graph.sh
+++ filter-with-graph.sh
@@ -6,16 +6,7 @@ scale
 photosensitivity=${params},
 split[P][I];
 
-[I]drawgraph
-	=fg1=0x00FFFF:m1=lavfi.photosensitivity.fixed-badness
-	:fg2=0x0000FF:m2=lavfi.photosensitivity.badness
-	:fg3=0xFF00FF:m3=lavfi.photosensitivity.frame-badness
-	:fg4=0x00FF00:m4=lavfi.photosensitivity.factor
-	:min=0
-	:max=2
-	:slide=scroll
-	:bg=0x000000
-	:mode=line
+[I]$(cat filter-drawgraph.txt)
 	:size=640x100
 [G];
 
